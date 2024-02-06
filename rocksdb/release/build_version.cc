19fc3a8ba8ddfc7e1d6b648c434153d8da501151
yanxwang <yanxwang@outlook.com>
2024-02-05 21:26:08 -0600

    first compile

---
+++ rocksdb/release/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2024/02/06 03:07:59@";
+const char* rocksdb_build_compile_date = __DATE__;
